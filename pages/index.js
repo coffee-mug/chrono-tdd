12894a8cb9f1561b2c69d725d9c68b9a69815e24
Lucas Kostka <lucas.kostka@escdijon.eu>
2019-09-03 15:24:56 +0200

    Chrono page is now the index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import useInterval from "../hooks/userInterval";
 
-function IndexComponent() {
+function ChronoComponent() {
     const [count, setCount] = useState(0);
+    const [delay, setDelay] = useState(1000);
+    const [isPaused, setPaused] = useState(false);
+
+    useInterval(() => {
+        setCount(count + 1);
+    }, delay);
+
+    // pause pauses the chrono
+    function pause() {
+        if (isPaused) {
+            setDelay(1000);
+        } else {
+            setDelay(null);
+        }
+        setPaused(!isPaused);
+    }
+
+    function restart() {
+        setCount(0);
+    }
 
     return (
-        <div>{ count }</div>
+        <div>
+            <div className="chrono">{count}</div>
+            <button onClick={pause} className="pause">{ isPaused ? 'Restart' : 'Pause' }</button>
+            <button onClick={restart} className="reset">Reset</button>
+        </div>
     )
 }
 
-
-export default IndexComponent
+export default ChronoComponent;
